ee929db0264e8bffd9ccb5c0519bfdb313ca4b0b
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2021-03-14 21:56:52 +0100

    fix requirements badges breaking mobile layout

--- src/components/blueprints_docs/requirements/CustomRequirement.jsx
+++ src/components/blueprints_docs/requirements/CustomRequirement.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Badge } from 'react-bootstrap'
 
+const styles = {
+  requirementNameContainer: {
+    paddingBottom: '0.8rem',
+  },
+  requirementName: {
+    display: 'inline',
+  },
+}
+
 function Requirement(props) {
   return (
     <div className='mb-3'>
-      <h3>
-        {props.name}{' '}
+      <div style={styles.requirementNameContainer}>
+        <h3 style={styles.requirementName}>{props.name} </h3>
         {props.required ? (
           <Badge variant='warning'>{props.required} Required</Badge>
         ) : (
           <Badge variant='info'>Optional</Badge>
         )}
-      </h3>
+      </div>
       {props.children}
     </div>
   )
