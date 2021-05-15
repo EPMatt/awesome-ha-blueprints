896d9d00c346e89a87d5d4468c687e38c7e4b090
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2021-05-16 00:16:25 +0200

    improve documentation controllers list

---
+++ src/components/ControllerImage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ControllerImage(props) {
+  return <img style={{ width: 200 }} size='2000' src={`/static/img/controllers/${props.img}`} />
+}
+
+export default ControllerImage
