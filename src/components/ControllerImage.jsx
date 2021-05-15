ade78fff2baeec23c909a7434dd11b0231f9d61f
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2021-05-16 00:58:40 +0200

    fix controller images

--- src/components/ControllerImage.jsx
+++ src/components/ControllerImage.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 
-function ControllerImage({ img }) {
-  return (
-    <img
-      style={{ width: 200 }}
-      size='2000'
-      src={`/static/img/controllers/${img}`}
-      alt={img}
-    />
-  )
+function ControllerImage({ children }) {
+  return <div style={{ width: 200 }}>{children}</div>
 }
 
 export default ControllerImage
