59b4617398f6af90a8a26ef9e57d6d2ec8d85871
EPMatt <30753195+EPMatt@users.noreply.github.com>
2025-03-18 17:22:43 +0000

    chore: fix tsconfig and eslint config, remove old babel config

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -3,15 +3,20 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import prettierEslint from 'eslint-config-prettier'
-
+import { globalIgnores } from "eslint/config"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  globalIgnores([".docusaurus/", "build/"]),
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  pluginReact.rules["react-in-jsx-scope"],
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  },
   prettierEslint,
 ];
