cbd602e6bb923b472770694393c06481c0db3edd
Matteo Agnoletto <info@epmatt.com>
2025-03-26 14:59:11 +0100

    feat(docs): automatic controller lists with search features (#738)
    * feat: implement v0 controllers list
    
    * feat: automatic number of controllers and vendors
    
    * feat: search by manufacturer and name
    
    * docs(controllers): fix integration names casing

--- website/src/components/blueprints_docs/index.tsx
+++ website/src/components/blueprints_docs/index.tsx
@@ -3,5 +3,13 @@ import Inputs from './Inputs'
 import Requirement from './Requirement'
 import ImportCard from './BlueprintImportCard'
 import BlueprintsList from './BlueprintsList'
+import ControllersList from './ControllersList'
 
-export { Input, Inputs, Requirement, ImportCard, BlueprintsList }
+export {
+  Input,
+  Inputs,
+  Requirement,
+  ImportCard,
+  BlueprintsList,
+  ControllersList,
+}
