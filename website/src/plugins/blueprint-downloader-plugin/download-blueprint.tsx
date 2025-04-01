49df2567c7ec09adff084b56da7334cd0f927132
EPMatt <30753195+EPMatt@users.noreply.github.com>
2025-04-01 13:37:59 +0000

    feat: correct head title

--- website/src/plugins/blueprint-downloader-plugin/download-blueprint.tsx
+++ website/src/plugins/blueprint-downloader-plugin/download-blueprint.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react'
 // @ts-expect-error no types for this
 import { useLocation } from '@docusaurus/router'
+// @ts-expect-error no types for this
+import Head from '@docusaurus/Head'
 
-export default function DownloadBlueprint() {
+export default function DownloadBlueprint(props: {
+  route: { category: string; id: string }
+}) {
   const location = useLocation()
+  const category = props.route.category
+  const id = props.route.id
 
   useEffect(() => {
     // Get query parameters
     const searchParams = new URLSearchParams(location.search)
-    // Extract category and id from the URL path
-    const pathParts = location.pathname.split('/')
-    const category = pathParts[pathParts.length - 2]
-    const id = pathParts[pathParts.length - 1]
     const version = searchParams.get('version') || 'latest'
     // Sanitize version - only allow 0-9, dot and a-z
     // eslint-disable-next-line
@@ -30,6 +32,11 @@ export default function DownloadBlueprint() {
 
   return (
     <>
+      <Head>
+        <title>
+          Download {category}/{id}
+        </title>
+      </Head>
       <div
         style={{
           display: 'flex',
